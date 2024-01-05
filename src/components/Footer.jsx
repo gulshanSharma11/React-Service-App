7186661406fb3805a50975cb9cc29e86b2c9aa9e
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-01-05 11:59:42 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
       <div>
         <h5>Follow Us</h5>
         <div>
-          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+         
             Youtube
           </a>
-          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+          
             Instagram
           </a>
-          <a href="https://github.com/meabhisingh" target={"blank"}>
+          <a href="https://github.com/gulshanSharma11" target={"blank"}>
             Github
           </a>
         </div>
